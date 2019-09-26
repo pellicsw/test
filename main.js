c3ffaa57060e286749ecd6ff4fb2c6a2c3347234
pellicsw <55772583+pellicsw@users.noreply.github.com>
2019-09-26 14:50:32 +0900

    drgdffdgfdgdgdfgfdgdgf

---
+++ main.js
@@ -0,0 +1,14 @@
+consol log( "main.js loded")
+$(document). ready(function(){
+    'use strict';
+    paper.install(window);
+    paper. setup( document.getElementById( 'maincanvas') );
+    var c;
+    for (var x = 25; x < 400; x += 50){
+        for (var x = 25; x < 400; x += 50){
+        c = shape.circle( x, y, 20);
+         c.fillcolor = green  ;
+    }
+}
+var tool =new tool();
+var c = shape.circle( 200, 200, 80);
